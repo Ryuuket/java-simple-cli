8634440beb50232dc9ce1b22fcace5a2fe81d567
Axel Galvier <axelgalvier0@gmail.com>
2023-10-13 16:19:31 +0200

    Clean first part of the project.

---
+++ Cli.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;      
+
+public class Cli {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in); // Listen to the standard input (console)
+		System.out.print("> "); // Prompt
+		while (true) { // Infinite loop
+			String command = scanner.nextLine(); // Get input from console as an array of strings
+			String output = ""; // A variable named output of type String
+			if (command.equals("exit")) {
+				break; // Forces exit of the while loop
+			} 
+			switch(command) {
+				case "date":
+					output = java.time.LocalDate.now().toString();
+					break;  
+				case "time":
+					output = java.time.LocalTime.now().toString();
+					break;
+				case "datetime":
+					output = java.time.LocalDateTime.now().toString();
+					break;
+				case "useraccount":
+					output = System.getProperty("user.name");
+					break;
+				case "userhome":
+					output = System.getProperty("user.home");
+					break;
+				case "os":
+					output = System.getProperty("os.name");
+					break;
+				default:
+					if (command.startsWith("printenv")) {
+						String[] strTable = splitString(command, 0);
+						output = System.getenv(strTable[1]);
+						break;
+					}
+					if (command.startsWith("echo")) {
+						String[] strTable = splitString(command, 0);
+						for(int i = 1; i < strTable.length; i++) {
+							output += strTable[i] + " ";
+						}
+						break;
+					}
+					// String concatenation
+					output = "Command '" + command + "' not found.";
+			}
+			System.out.println(output); // Print with new line (ln)
+			System.out.print("> "); // Prompt
+		}
+		scanner.close(); // Best practice, always close a stream when no more needed
+		System.out.println("Bye!");
+	}
+
+	public static String[] splitString(String str, int number) {
+		return str.split(" ", number);
+	}
+}
