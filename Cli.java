5052e310590886708d094c05fbb4ea6267afece4
Axel Galvier <axelgalvier0@gmail.com>
2023-10-17 12:01:25 +0200

    First prototype of part 2 CLI.

--- Cli.java
+++ Cli.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.time.LocalDateTime;  
 import java.time.format.DateTimeFormatter;      
@@ -10,8 +12,8 @@ public class Cli {
 			String command[] = scanner.nextLine().split(" ", 2); // Get input from console as an array of strings
 			String output = ""; // A variable named output of type String
 			switch(command[0]) {
-				case "date":
-					output = java.time.LocalDate.now().toString();
+				case "date": 
+					output = java.time.LocalDate.now().toString();  
 					break;  
 				case "time":
 					output = java.time.LocalTime.now().toString();
@@ -29,16 +31,40 @@ public class Cli {
 					output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")";
 					break;
 				case "printenv":
-					if(command.length > 1 && System.getenv(command[1]) != null) {
+					if(command.length == 1) {
+						var wrapper = new Object(){ String value = ""; };
+						System.getenv().forEach((variable, value) -> {
+    							wrapper.value += variable + ":" + value + "\n";
+						});
+						output = wrapper.value;
+					} else if(command.length > 1) {
 						output = System.getenv(command[1]);
+						if(output == null) {
+							output = "";
+						}
 					}
 					break;
 				case "echo":
+				case "print":
 					if(command.length > 1) {
 						output = command[1];
 					}
 					break;
+				case "ls":
+					if(command.length > 1) {
+						File path = new File(command[1]);
+						String contents[] = path.list();
+						if(contents.length > 0) {
+							for(int i=0; i<contents.length; i++) {
+								output += contents[i] + "\n";
+							} 
+						} else {
+							output = "Not a directory.";
+						}
+					}
+					break;
 				case "exit": 
+				case "logout":
 					break loop; // Forces exit of the while loop
 				default:
 					// String concatenation
